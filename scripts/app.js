c49f35fc40a77d8ae8754eac527b808550b6593d
fathimadeenaz <63476604+fathimadeenaz@users.noreply.github.com>
2022-04-30 13:31:04 +0000

    Deploying to gh-pages from @ fathimadeenaz/Wordle-Clone@d677bbea4f91a42e0a1edfb3131fb29de862423e 🚀

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const boardDisplay = document.querySelector('#board-container')
-const keys = document.querySelectorAll(".keyboard-row button");
+const keys = document.querySelectorAll('.keyboard-row button')
+const messageDisplay = document.getElementById('message-container')
 
 // console.log(boardDisplay)
 
@@ -42,9 +43,9 @@ window.addEventListener('keyup', (e) => handleKeyUp(e.key))
 window.addEventListener('keypress', (e) => handleKeyPress(e.key))
 
 const handleKeyPress = (key) => {
-    console.log(key);
+    // console.log(key);
     if(key.keyCode == 9){
-        return false;
+        key.preventDefault()
     }
 }
 
@@ -85,7 +86,8 @@ const addLetter = (letter) => {
     if (currentTile < 5 && currentRow < 6) {
         const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
         tile.textContent = letter
-        tile.style.borderColor = '#878a8c'
+        // tile.style.borderColor = '#878a8c'
+        tile.classList.add('tile-active')
         guessRows[currentRow][currentTile] = letter.toUpperCase()
         currentTile++
     }
@@ -96,7 +98,8 @@ const deleteLetter = () => {
         currentTile--
         const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
         tile.textContent = ''
-        tile.style.borderColor = ''
+        // tile.style.borderColor = ''
+        tile.classList.remove('tile-active')
         guessRows[currentRow][currentTile] = ''
     }
 }
@@ -112,22 +115,34 @@ const checkRow = () => {
         colorKey()
 
         if (wordle == guess) {
-            console.log('you\'ve guessed the wordle!')
             isGameOver = true
+            showMessage('Superb')
+            // console.log('you\'ve guessed the wordle!')
             return
         } else {
             if (currentRow >= 5) {
                 isGameOver = true
-                console.log('game over \nthe wordle was',wordle)
+                showMessage(wordle)
+                // console.log('game over \nthe wordle was',wordle)
                 return
             }
             if (currentRow < 5) {
                 currentRow++
                 currentTile = 0
-                console.log('try having another guess')
+                showMessage('Try Again')
+                // console.log('try having another guess')
             }
         }
     }
+}
+
+const showMessage = (message) => {
+    messageDisplay.style.visibility = 'visible'
+    // messageDisplay.style.display = 'block'
+    const messageElement = document.createElement('p')
+    messageElement.textContent = message
+    messageDisplay.append(messageElement)
+    if(!isGameOver) setTimeout(() => messageDisplay.removeChild(messageElement), 1500)
 }
 
 const addClass = (arr, className) => {
@@ -186,43 +201,47 @@ const colorTile = (guessRow) => {
 
 const colorKey = () => {
     for(let i = 0; i < 5; i++) {
+
+        console.log(toggle);
         
-        const green = 'rgb(106, 170, 100)'
-        const yellow = 'rgb(201, 180, 88)'
-        const grey = 'rgb(120, 124, 126)'
+        const green = toggle ? 'rgb(106, 170, 100)' : 'rgb(83, 141, 78)'
+        const yellow = toggle ? 'rgb(201, 180, 88)' : 'rgb(181, 159, 59)'
+        const grey = toggle ? 'rgb(120, 124, 126)' : 'rgb(58, 58, 60)'
 
 
-        let colorClass = document.querySelector(getTile(i)).className.slice(5)
+        let colorClass = document.querySelector(getTile(i)).className.slice(17)
         let letter = document.querySelector(getTile(i)).textContent
+        let key = document.querySelector(`#${letter}`)
+
         // console.log(colorClass)
         // console.log(letter)
 
-        let newColor = colorClass === 'present' ? yellow : (colorClass === 'correct' ? green : grey)
+        // let newColor = colorClass === 'present' ? yellow : (colorClass === 'correct' ? green : grey)
         // console.log(newColor)
 
         // let oldColor = document.querySelector(`#${letter}`).style.backgroundColor
-        let oldColor = window.getComputedStyle(document.querySelector(`#${letter}`)).backgroundColor
+        // let oldColor = window.getComputedStyle(document.querySelector(`#${letter}`)).backgroundColor
         // console.log(`oldColor ${oldColor}`)
 
 
-        if (oldColor === green) {
-            newColor = oldColor
+        if (colorClass === 'correct') {
+            key.classList.add('correct-key')
         } 
 
-        if (oldColor === yellow) {
-            if(newColor === green) newColor = green
-            else newColor = oldColor
+        if (colorClass === 'present') {
+            if (key.classList.contains('correct-key')) {}
+            else key.classList.add('present-key')
         }
 
-        if (oldColor === grey) {
-            if(newColor === green) newColor = green
-            else if(newColor === yellow) newColor = yellow
-            else newColor = oldColor
+        if (colorClass === 'absent') {
+            if (key.classList.contains('correct-key')) {}
+            else if (key.classList.contains('present-key')) {}
+            else key.classList.add('absent-key')
         }
 
         // console.log(`newColor ${newColor}`)
-        document.querySelector(`#${letter}`).style.backgroundColor = newColor
-        document.querySelector(`#${letter}`).style.color = 'rgb(255,255,255)'
+        // document.querySelector(`#${letter}`).style.backgroundColor = newColor
+        // document.querySelector(`#${letter}`).style.color = 'rgb(255,255,255)'
         // console.log(window.getComputedStyle(document.querySelector(`#${letter}`)).color);
     }
 }
@@ -233,16 +252,17 @@ const animate = gsap.timeline({ paused: true });
 const animateBackground = new TimelineMax({ paused: true });
 let toggle = true;
 
-animateBackground
-    .to("body", 0.1, { backgroundColor: "#000" }, 0.2)
-    .to("h1", 0.1, { color: "#FFF" }, 0.2)
-    .to("path", 0.1, { fill: "#FFF" }, 0.2)
-    .to("#title-container", 0.1, { borderColor: "#3A3A3C" }, 0.2)
-    .to("button", 0.1, { backgroundColor: "#818384", color: "#FFF" }, 0.2)
-    .to(".tile", 0.1, { /*borderColor: "#3A3A3C",*/ color: "#FFF" }, 0.2)
-    // .to(".correct", 0.1, { backgroundColor: "#538D4E", color: "#FFF", borderColor: "#538D4E" }, 0.2)
-    // .to(".present", 0.1, { backgroundColor: "#B59F3B", color: "#FFF", borderColor: "#B59F3B" }, 0.2)
-    // .to(".absent", 0.1, { backgroundColor: "#3A3A3C", color: "#FFF", borderColor: "#3A3A3C" }, 0.2)
+// animateBackground
+    // .to("body", 0.1, { backgroundColor: "#000" }, 0)
+    // .to("h1", 0.1, { color: "#FFF" }, 0)
+    // .to("path", 0.1, { fill: "#FFF" }, 0)
+    // .to("#title-container", 0.1, { borderColor: "#3A3A3C" }, 0)
+    // .to("button", 0.1, { color: "#FFF" }, 0)
+    // .to(".tile", 0.1, { borderColor: "#3A3A3C", color: "#FFF" }, 0)
+
+    // .set(".correct", { backgroundColor: "#538D4E", color: "#FFF", borderColor: "#538D4E" })
+    // .set(".present", { backgroundColor: "#B59F3B", color: "#FFF", borderColor: "#B59F3B" })
+    // .set(".absent", { backgroundColor: "#3A3A3C", color: "#FFF", borderColor: "#3A3A3C" })
     
 
 
@@ -256,8 +276,12 @@ animate
     .to(".toggle-button", 0.2, { scale: 1 })
 
 document.getElementsByClassName("switch")[0].addEventListener("click", () => {
+
+    document.querySelector('body').classList.toggle('dark-theme')
+
+
     if(toggle){
-        console.log('in dark mode');
+        // console.log('in dark mode');
         animate.restart();
         animateBackground.restart();
     } else {
@@ -266,3 +290,8 @@ document.getElementsByClassName("switch")[0].addEventListener("click", () => {
     }
     toggle = !toggle;
 });
+
+
+// document.querySelector('.container').addEventListener('click', () => {
+//     document.querySelector('body').classList.toggle('dark-theme')
+// })
